15c7ba525073f7c0ddfd96b6f5965cf0dc92af7a
Andrej Spilak <andrej.spilak@gmail.com>
2018-01-25 22:46:12 +0100

    answering questions, trying things out

---
+++ CSharp.Dribble.Exam70483/Assorted/JamSessions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Dribble.Exam70483.Assorted
+{
+    public class MyEnumerable : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MyEnumerator : IEnumerator
+    {
+        public object Current => throw new NotImplementedException();
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSharp.Dribble.Exam70483/CreateAndUseTypes/DelegateCoAndContraVariance.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Dribble.Exam70483.CreateAndUseTypes
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
+    public class Employee : Person
+    {
+    }
+
+    /// <summary>
+    /// Used to dempnstrate covariance.
+    /// </summary>
+    public delegate Person GetPersonDelegate();
+
+    /// <summary>
+    /// Used to demonstrate contravariance
+    /// </summary>
+    public delegate void PassEmployeeDelegate(Employee e);
+
+
+    public class DelegateCoAndContraVariance
+    {
+        public Employee GetEmployee() => new Employee();
+
+        public void WorkWithPerson(Person p) { }
+
+
+        public void Covariance()
+        {
+            GetPersonDelegate d = GetEmployee;
+            //d += GetEmployee;
+        }
+
+        public void Contravariance()
+        {
+            PassEmployeeDelegate d = WorkWithPerson;
+        }
+    }
+
+    public class TestBase
+    {
+        public event EventHandler EventName;
+
+        protected void OnEvent()
+        {
+            if (EventName != null)
+            {
+                EventName(this, EventArgs.Empty);
+            }
+        }
+    }
+    public class Test : TestBase
+    {
+        void Method()
+        {
+            EventArgs args = EventArgs.Empty;
+
+            // THIS WON'T WORK! You can't invoke inherited event directly
+            //if (base.EventName != null) 
+                //base.EventName(this, args);
+        }
+    }
+}
